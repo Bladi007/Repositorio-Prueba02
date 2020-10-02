0a4b47b5ec96724b91fc42ddb08a4aa9e36de42f
Bladi007 <53631048+Bladi007@users.noreply.github.com>
2020-10-02 16:43:09 -0400

    Add files via upload

---
+++ Proyecto Prueba 05/Prpyecto_Prueba/Prpyecto_Prueba/Entities/Usuario.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Prpyecto_Prueba.Entities
+{
+    public class Usuario
+    {
+
+        public string User_Id { get; set; }
+        public string User_Nombre { get; set; }
+        public string User_Apellido { get; set; }
+        public string User_Correo { get; set; }
+    }
+}
